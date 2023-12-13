fc00f9bb4fb26a8df9cc7cbb19b72cb0ad4a8699
Suzzzzzy <suji9709@gmail.com>
2023-12-13 16:54:11 +0900

    add hash map memo

--- map/map_test.go
+++ map/map_test.go
@@ -26,3 +26,23 @@ func TestHashMap(t *testing.T) {
 	assert.Equal(t, 300, val)
 
 }
+
+func TestGoBasicMap(t *testing.T) {
+	m := make(map[string]int) //map[key type]value type
+	m["tucker"] = 100
+	m["golang"] = 200
+	m["awesome"] = 300
+
+	assert.Equal(t, 100, m["tucker"])
+	assert.Equal(t, 200, m["golang"])
+	assert.Equal(t, 300, m["awesome"])
+	assert.Equal(t, 0, m["aaa"]) // 없는 값을 조회했을 때는 int의 default값 0이 나온다
+
+	_, ok := m["aaa"]   // 이런식으로 값이 존재하는지 알 수 있다 ok
+	assert.False(t, ok) // 값이 없기때문에 false 나옴
+
+	delete(m, "tucker") // delete를 사용해서 맵에서 삭제 가능
+	val, ok := m["tucker"]
+	assert.False(t, ok)
+	assert.Equal(t, val, 0)
+}
